15b465ea4312257d6040e0c786d1406919c43a25
Samuel Cox <samc1253@gmail.com>
2023-03-05 16:54:54 -0700

    Add favorites to schema

---
+++ db/migrate/20230305235327_create_favorites.rb
@@ -0,0 +1,10 @@
+class CreateFavorites < ActiveRecord::Migration[5.2]
+  def change
+    create_table :favorites do |t|
+      t.references :user, foreign_key: true
+      t.string :country
+      t.string :recipe_link
+      t.string :recipe_title
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_04_205341) do
+ActiveRecord::Schema.define(version: 2023_03_05_235327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "country"
+    t.string "recipe_link"
+    t.string "recipe_title"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +29,5 @@ ActiveRecord::Schema.define(version: 2023_03_04_205341) do
     t.string "api_key"
   end
 
+  add_foreign_key "favorites", "users"
 end
