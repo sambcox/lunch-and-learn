04a2226ce8f0aa773e131b054d6092248849fd5f
Samuel Cox <samc1253@gmail.com>
2023-03-06 11:24:44 -0700

    Adjust assignment of tourist sight address

--- app/poros/tourist_sight.rb
+++ app/poros/tourist_sight.rb
@@ -4,11 +4,7 @@ class TouristSight
   def initialize(data)
     @id = nil
     @name = data[:name]
-    @address = get_address(data)
+    @address = data[:formatted]
     @place_id = data[:place_id]
-  end
-
-  def get_address(data)
-    [data[:address_line1], data[:address_line2]].join(', ')
   end
 end
