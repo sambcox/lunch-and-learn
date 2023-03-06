ebc49f15c5d2ca757050b85b25ae964c3959a927
Samuel Cox <samc1253@gmail.com>
2023-03-05 17:16:55 -0700

    Add happy path for favorite creation

---
+++ app/controllers/api/v1/favorites_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::FavoritesController < ApplicationController
+  def create
+    user = User.find_by_api_key(params[:api_key])
+
+    favorite = user.favorites.new(favorite_params)
+
+    if favorite.save
+      render json: { success: 'Favorite added successfully' }, status: :created
+    else
+      render json: ErrorSerializer.serialize(Error.new(user.errors)), status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def favorite_params
+    params.permit(:country, :recipe_link, :recipe_title)
+  end
+end
---
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Favorite Requests' do
+  describe 'favorite post requests' do
+    let(:user) { User.create!(name: 'Samuel Cox', email: 'samuel@example.com') }
+    it 'can create a favorite for a user' do
+      favorite_params = {
+        api_key: user.api_key,
+        country: 'thailand',
+        recipe_link: 'https://www.testurl.com',
+        recipe_title: 'Test Favorite'
+      }
+      headers = { 'CONTENT_TYPE' => 'application/json'}
+      post api_v1_favorites_path, headers: headers, params: JSON.generate(favorite_params)
+
+      expect(response).to be_successful
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_response[:success]).to eq('Favorite added successfully')
+    end
+  end
+end
