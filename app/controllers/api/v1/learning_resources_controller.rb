a005ca14cf3466da19e02c8923a14486b629c463
Samuel Cox <samc1253@gmail.com>
2023-03-06 16:08:25 -0700

    Add country validation to learning resources

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,12 +1,18 @@
 class Api::V1::LearningResourcesController < ApplicationController
+  before_action :confirm_country
   def index
-    raise BadDataError, 'Country must be provided' unless params[:country]
-
     resource = LearningResourceFacade.find_resources(learning_resource_params[:country])
     render json: LearningResourceSerializer.new(resource)
   end
 
   private
+
+  def confirm_country
+    raise BadDataError, 'Country must be provided' unless params[:country]
+    return if CountryFacade.valid_country?(learning_resource_params[:country])
+
+    raise BadDataError, 'Country provided must be a valid country'
+  end
 
   def learning_resource_params
     params.permit(:country)
